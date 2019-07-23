884a83c3c1833e496feed33801bde27285b1aa30
Steven Kang <skang28@gmail.com>
2019-07-22 20:27:57 -0400

    auto scroll changes

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+$(function() {
+
 $('.menu-toggle').click(function(event) {
     event.preventDefault();
     $('.rightSideLinks').toggleClass('hamburger')
 });
 
-$('.scrollLinks').click(function(event) {
-    $('html, body').animate({ scrollTop: 500 }, 2000);
+$('.scrollLinks').on('click','a',function(event) { 
+    $('html, body').animate({ scrollTop: $('.'+$(event.currentTarget).data().scrollTarget).offset().top }, 500); });
+
 });
