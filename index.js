3aede2dca5a5c4734ca224c9cee9b1145c374572
Steven Kang <skang28@gmail.com>
2019-07-23 23:01:35 -0400

    more hamburger menu edits

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ $(function() {
 
 $('.menu-toggle').click(function(event) {
     event.preventDefault();
-    $('.leftLinks','.rightLinks').toggleClass('hamburger')
+    $('ul').toggleClass('hamburger')
+    $('nav').toggleClass('hamburger')
 });
 
 $('.rightLinks').on('click','a',function(event) { 
-    $('html, body').animate({ scrollTop: $('.'+$(event.currentTarget).data().scrollTarget).position().top - 60 }, 500); });
+    $('html, body').animate({ scrollTop: $('.'+$(event.currentTarget).data().scrollTarget).offset().top - 60}, 500); });
+
 
 });
