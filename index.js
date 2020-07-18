3d07783a3d5a8c6af2ec539f0ad5f9e458149d34
Steven Kang <skang28@gmail.com>
2020-07-17 20:55:51 -0400

    updating look through jquery

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ $('.menu-toggle').click(function(event) {
     $('nav').toggleClass('hamburger')
 });
 
-$('.rightLinks').on('click','a',function(event) { 
-    $('html').animate({ scrollTop: $('.'+$(event.currentTarget).data().scrollTarget).offset().top - 50}, 500); });
-
+$('.nav').find("a").click(function() {
+    $(".active").removeClass("active");
+    $(this).addClass("active");
+    $(".pactive").fadeOut(200, function() {
+        $(this).removeClass("pactive");
+    });
+    $("#p" + this.id).fadeIn(200, function() {
+        $(this).addClass("pactive");
+    });
+});
 
 });
