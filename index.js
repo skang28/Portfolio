dfe89a0b4706e0cadb53896bece438673153618f
Steven Kang <skang28@gmail.com>
2020-09-01 20:32:34 -0400

    updated portfolio

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ $('.menu-toggle').click(function(event) {
     $('nav').toggleClass('hamburger')
 });
 
-$('.nav').find("a").click(function() {
+$("ul").find("a").click(function() {
     $(".active").removeClass("active");
     $(this).addClass("active");
     $(".pactive").fadeOut(200, function() {
